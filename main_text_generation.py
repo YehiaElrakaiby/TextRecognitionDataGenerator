f94eb0017fccf679f0c33928a340e8108d7b20b7
YehiaElrakaiby <yehia.elrakaiby@gmail.com>
2023-03-17 14:21:24 +0100

    Update main_text_generation.py

--- main_text_generation.py
+++ main_text_generation.py
@@ -13,6 +13,7 @@ class MyTextGenerator:
                  out_dir_path: str,
                  nb_of_words: int,
                  language: str,
+                 image_dir: str,  # path to a folder where background images are kept
                  from_dict: bool = False):
         output_ground_truth_file = f'{out_dir_path}/gt/gt.json'
 
@@ -39,7 +40,8 @@ class MyTextGenerator:
                 text_color='#000000,#888888',
                 fit=True,
                 size=64,
-                background_type=2
+                # background_type=2
+                image_dir=image_dir
             )
         else:
             generator = GeneratorFromStrings(
@@ -54,7 +56,8 @@ class MyTextGenerator:
                 text_color='#000000,#888888',
                 fit=True,
                 size=64,
-                background_type=2
+                # background_type=2
+                image_dir=image_dir
             )
         ground_truth = {}
         for img, lbl in generator:
@@ -62,10 +65,14 @@ class MyTextGenerator:
             image_name: str = lbl
             image_name = image_name.replace("/", "")
             image_file_path = os.path.join(out_dir_path, f"{image_name}_{counter}.jpg")
-            ground_truth[str(image_file_path)] = lbl
 
-            img.save(image_file_path)
-            img.close()
+            try:
+                img.save(image_file_path)
+                img.close()
+                ground_truth[str(image_file_path)] = lbl
+            except AttributeError as e:
+                print(f'Error saving image {image_file_path} with label {lbl}')
+
             counter += 1
 
         # Serializing json
@@ -80,9 +87,11 @@ if __name__ == '__main__':
     current_directory_o = os.getcwd()
     words_file_path_o = f'{current_directory_o}/trdg/dicts/keras.txt'
     output_directory_o = f'{current_directory_o}/out'
+    image_dir_o = f'{current_directory_o}/trdg/images'
 
     bb = MyTextGenerator(input_words_file_path=words_file_path_o,
                          out_dir_path=output_directory_o,
                          nb_of_words=3000,
                          language='keras',
+                         image_dir=image_dir_o,
                          from_dict=True)
