a6c8e60945f3de1d119585f0ec79edc38853bb1d
YehiaElrakaiby <yehia.elrakaiby@gmail.com>
2023-03-17 15:08:28 +0100

    Update main_text_generation.py

--- main_text_generation.py
+++ main_text_generation.py
@@ -9,12 +9,26 @@ from trdg.generators import (
 
 
 class MyTextGenerator:
+
     def __init__(self, input_words_file_path: str,
                  out_dir_path: str,
-                 nb_of_words: int,
-                 language: str,
+                 nb_of_sentences: int,
+                 language: str,  # ['keras', 'numbers']
                  image_dir: str,  # path to a folder where background images are kept
                  from_dict: bool = False):
+        """
+        Generates synthetic images and a grond truth file containing a JSON dictionary mapping the path of the image
+        to its label (the sentence)
+
+        @param input_words_file_path: the words using which to compose sentences
+        @param out_dir_path: where to save the images and the ground truth file
+        @param nb_of_sentences: the number of sentences to generate
+        @param language: the language (dictionary) to use for generation of sentences
+        @param image_dir: the directory where background images are stored
+        @param from_dict: specifies whether to generate sentences from the dict corresponding to the language
+            or generate words using the input_words_file_path
+        """
+
         output_ground_truth_file = f'{out_dir_path}/gt/gt.json'
 
         counter = 0
@@ -31,13 +45,12 @@ class MyTextGenerator:
             generator = GeneratorFromDict(
                 length=3,
                 language=language,  # ['keras', '7led']
-                count=nb_of_words,
+                count=nb_of_sentences,
                 blur=2,
                 distorsion_type=3,
                 random_blur=True,
                 random_skew=True,
                 skewing_angle=15,
-                text_color='#000000,#888888',
                 fit=True,
                 size=64,
                 # background_type=2
@@ -47,7 +60,7 @@ class MyTextGenerator:
             generator = GeneratorFromStrings(
                 strings=words_list,
                 language=language,  # ['keras', '7led']
-                count=nb_of_words,
+                count=nb_of_sentences,
                 blur=2,
                 distorsion_type=3,
                 random_blur=True,
@@ -64,16 +77,20 @@ class MyTextGenerator:
             # Do something with the pillow images here.
             image_name: str = lbl
             image_name = image_name.replace("/", "")
-            image_file_path = os.path.join(out_dir_path, f"{image_name}_{counter}.jpg")
+            identifier = str(counter)
+
+            identifier = identifier.zfill(5)
+
+            image_file_path = os.path.join(out_dir_path, f"{identifier}_{image_name}.jpg")
 
             try:
                 img.save(image_file_path)
                 img.close()
                 ground_truth[str(image_file_path)] = lbl
-            except AttributeError as e:
-                print(f'Error saving image {image_file_path} with label {lbl}')
+                counter += 1
 
-            counter += 1
+            except AttributeError as e:
+                print(f'Skipped saving image {image_file_path} with label {lbl}: {e}')
 
         # Serializing json
         json_object = json.dumps(ground_truth, indent=4)
@@ -87,11 +104,11 @@ if __name__ == '__main__':
     current_directory_o = os.getcwd()
     words_file_path_o = f'{current_directory_o}/trdg/dicts/keras.txt'
     output_directory_o = f'{current_directory_o}/out'
-    image_dir_o = f'{current_directory_o}/trdg/images'
+    image_dir_o = f'{current_directory_o}/trdg/images/dark'
 
     bb = MyTextGenerator(input_words_file_path=words_file_path_o,
                          out_dir_path=output_directory_o,
-                         nb_of_words=3000,
+                         nb_of_sentences=3000,
                          language='keras',
                          image_dir=image_dir_o,
                          from_dict=True)
